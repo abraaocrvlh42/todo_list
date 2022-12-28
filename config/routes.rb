0a89b150f479fe8e32d6eaaf50b9fe2909bd21e7
abraaocrvlh42 <abraaoncrvlh@gmail.com>
2022-12-28 17:36:37 -0300

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :tasks, only: [:index, :create, :update, :destroy]
+  
+  root "tasks#index"
 end
